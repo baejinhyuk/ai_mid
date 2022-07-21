a1cf9e6cf163ffc3fea92b90cf6286f0d4fbf8be
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 00:12:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
-import koreanize_matplotlib
 
 st.set_page_config(
-    page_title="Midproject",
-    page_icon="",
+    page_title="Likelion AI School Dog Team Miniproject",
+    page_icon="🐶",
     layout="wide",
 )
+st.sidebar.markdown("# Stanford Dogs Data Info🐶")
 
+st.markdown('# CV Mini Project')
+st.markdown("## Stanford Dogs Dataset (kaggle) 🐶")
+st.markdown(
+    '#### [Stanford Dogs Dataset link](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
+st.markdown("강아지 사진을 올리면 무슨 종에 가까운지 알려줍니다!")
+
+st.markdown("![Alt Text](https://media.giphy.com/media/kxlDaFDVzTLb2/giphy.gif)")
+
+st.markdown('---')
+
+st.markdown('팀장: 주소리')
+st.markdown('팀원: 이주승, 김재석')
