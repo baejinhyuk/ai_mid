61e26b7b478afa70acd63a57adeed2337fc442f3
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 00:08:29 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+
+st.set_page_config(
+    page_title="Midproject",
+    page_icon="",
+    layout="wide",
+)
+
