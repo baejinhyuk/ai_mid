aafa95d2eb18e0c7f02c06f65f1646fd1d41dd5b
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:17:10 +0900

    Create data.py

---
+++ pages/data.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+root_dir = "VegetableImages/train"
+image_label = os.listdir(root_dir)
+image_label 
+
+code = root_dir = "VegetableImages/train"
+image_label = os.listdir(root_dir)
+image_label 
+st.code(code, language='python')
