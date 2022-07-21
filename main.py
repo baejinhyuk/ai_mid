f80bef9157e54de52cedecf5045207e4888e757b
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:58:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ st.set_page_config(
 st.sidebar.markdown("# Vegetable Image Dataset ")
 
 st.markdown('# Vegetable Image Dataset Mid Project')
+st.warning('## 전 세계에서 볼 수 있는 15가지 일반 야채로 CNN를 진행했습니다.')
 st.markdown("## 전 세계에서 볼 수 있는 15가지 일반 야채로 CNN를 진행했습니다.")
 st.markdown("야채는 콩, 여주, 박, 브리잘, 브로콜리, 양배추, 고추, 당근, 콜리플라워, 오이, 파파야, 감자, 호박, 무, 토마토입니다.")
 st.markdown(
@@ -17,7 +18,7 @@ st.markdown(
 
 st.markdown("![Alt Text](https://media.giphy.com/media/89sIi77nGsdnq/giphy.gif)")
 
-st.error('이거되면좋겠당')
+
 st.markdown('---')
 
 st.markdown('취업뽀개조 만세!')
