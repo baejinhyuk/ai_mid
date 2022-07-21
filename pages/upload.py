2ebed0fc4054ed2fe585a1a18c8b2ed404a31715
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 02:23:13 +0900

    Update upload.py

--- pages/upload.py
+++ pages/upload.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("GUESS WHAT DOG YOU ARE")
 
 st.warning('# 야채 15종 CNN')
+st.write("""
 # Put your picture and see what is your nearest breed!
 """)
 filename = st.file_uploader("Choose a file")
