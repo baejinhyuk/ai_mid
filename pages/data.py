5a1b05bbf8b225d6fa822171893539e5836c7351
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:22:15 +0900

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -1,17 +1,4 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import random
-import os
-import tqdm as tqdm
-from glob import glob
-import cv2
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
-from importlib.resources import path
 import streamlit as st
-from text.contents import *
 
 code = root_dir = "VegetableImages/train"
 image_label = os.listdir(root_dir)
