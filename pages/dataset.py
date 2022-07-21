208484a07052cf21d060dcd0fc9b7ead891f9664
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:02:33 +0900

    Update dataset.py

--- pages/dataset.py
+++ pages/dataset.py
@@ -9,7 +9,7 @@ st.code(code, language='python')
 
 
 st.markdown('### 이미지읽어오고 사이즈 조정')
-code1 = ''def img_read_resize(img_path):
+code1 = '''def img_read_resize(img_path):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (40, 40))
