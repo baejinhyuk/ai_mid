c0a1b730d5a7c674067112796c7034ffa97f71c5
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:46:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ st.set_page_config(
 st.sidebar.markdown("# Vegetable Image Dataset ")
 
 st.markdown('# Vegetable Image Dataset Mid Project')
-st.markdown("## 야채 사진 15종()")
+st.markdown("## 전 세계에서 볼 수 있는 15가지 일반 야채로 수행됩니다. 
+야채는 콩, 여주, 박, 브리잘, 브로콜리, 양배추, 고추, 당근, 콜리플라워, 오이, 파파야, 감자, 호박, 무, 토마토입니다. 15개 클래스의 총 21000개 이미지")
 st.markdown(
     '#### [Vegetable Image Dataset link](https://www.kaggle.com/datasets/misrakahmed/vegetable-image-dataset)')
     
