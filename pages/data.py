5528d2610b679ad7395898ea2d953741b1f5617e
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:24:41 +0900

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -14,3 +14,41 @@ code2 ='''def img_read_resize(img_path):
     return img '''
 
 st.code(code2, language='python')
+
+
+
+code3 ='''def img_folder_read(rt_dir, img_label):
+    img_files = []
+    labels = []
+    vfiles = glob(f"{rt_dir}/{img_label}/*")
+    vfiles = sorted(vfiles)
+    
+    for v_img in vfiles:
+        try:
+            img_files.append(img_read_resize(v_img))
+            labels.append(img_label)
+        except:
+            continue
+    
+    return img_files, labels
+
+img_sample, labels = img_folder_read("VegetableImages/train", "Tomato")
+len(img_sample), len(labels) '''
+
+st.code(code3, language='python')
+
+code4 ='''def imgs_to_array(rt_dir, image_label):
+    x_img = []
+    y_img = []
+    for img_label in tqdm.tqdm(image_label):
+        # 위에서 만든 img_folder_read 를 통해 
+        # 이미지와 정답값을 가져와서 train, test 리스트에 담아줍니다. 
+        x_temp, y_temp = img_folder_read(rt_dir, img_label)
+        x_img.extend(x_temp)
+        y_img.extend(y_temp)
+    return x_img, y_img
+x_train,y_train=imgs_to_array("VegetableImages/train", image_label)
+x_test,y_test=imgs_to_array("VegetableImages/test", image_label)
+x_val,y_val=imgs_to_array("VegetableImages/validation", image_label) '''
+
+st.code(code4, language='python')
