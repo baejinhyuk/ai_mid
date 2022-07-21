81f0f20354b547b9354d60b30605dbcb0738afc8
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:55:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ st.set_page_config(
 )
 st.sidebar.markdown("# Vegetable Image Dataset ")
 
-st.markdown('# 캐글주소: Vegetable Image Dataset Mid Project')
+st.markdown('# Vegetable Image Dataset Mid Project')
 st.markdown("## 전 세계에서 볼 수 있는 15가지 일반 야채로 CNN를 진행했습니다.")
 st.markdown("야채는 콩, 여주, 박, 브리잘, 브로콜리, 양배추, 고추, 당근, 콜리플라워, 오이, 파파야, 감자, 호박, 무, 토마토입니다.")
 st.markdown(
-    '#### [Vegetable Image Dataset link](https://www.kaggle.com/datasets/misrakahmed/vegetable-image-dataset)')
+    '#### 캐글주소: [Vegetable Image Dataset link](https://www.kaggle.com/datasets/misrakahmed/vegetable-image-dataset)')
     
 
 
