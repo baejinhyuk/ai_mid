496a6dc7564e975c53797216aac410e52ccfc9c0
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:23:47 +0900

    Update data.py

--- pages/data.py
+++ pages/data.py
@@ -5,3 +5,12 @@ image_label = os.listdir(root_dir)
 image_label '''
 
 st.code(code, language='python')
+
+
+code2 ='''def img_read_resize(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (40, 40))
+    return img '''
+
+st.code(code2, language='python')
