6e4050ab7a353f394682d5574849625b02dcb980
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 01:03:51 +0900

    Update dataset.py

--- pages/dataset.py
+++ pages/dataset.py
@@ -8,10 +8,34 @@ st.code(code, language='python')
 
 
 
-st.markdown('### 이미지읽어오고 사이즈 조정')
+st.markdown('### 이미지 읽고 사이즈 조정')
 code1 = '''def img_read_resize(img_path):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (40, 40))
     return imgl '''
 st.code(code1, language='python')
+
+st.markdown('### 파일분류 및 담아주기')
+code2 = '''def img_folder_read(rt_dir, img_label):
+    img_files = []
+    labels = []
+    vfiles = glob(f"{rt_dir}/{img_label}/*")
+    vfiles = sorted(vfiles)
+    
+    for v_img in vfiles:
+        try:
+            img_files.append(img_read_resize(v_img))
+            labels.append(img_label)
+        except:
+            continue
+    
+    return img_files, labels
+
+img_sample, labels = img_folder_read("VegetableImages/train", "Tomato")
+len(img_sample), len(labels) '''
+st.code(code2, language='python')
+
+
+
+
