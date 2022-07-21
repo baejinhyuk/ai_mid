14963221560cc09bc765f47ec082d24754b102bc
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 00:58:20 +0900

    Update dataset.py

--- pages/dataset.py
+++ pages/dataset.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from text.contents import *
+from importlib.resources import path
 
-st.markdown('취업뽀개조 만세!')
+st.markdown('### file load')
+st.code(image_labe, language='python')
+root_dir = "VegetableImages/train"
+image_label = os.listdir(root_dir)
+image_label 
