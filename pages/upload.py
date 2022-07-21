26cd32149336580a4ed5a9e417f4bddd8104f6a6
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 02:24:25 +0900

    Update upload.py

--- pages/upload.py
+++ pages/upload.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.warning('# 야채 15종 CNN')
 st.write("""
-# 15개의 정해진 야채중 사진을 올려주세요!
+### 15개의 정해진 야채중 사진을 올려주세요!
 """)
-filename = st.file_uploader("Choose a file")
+filename = st.file_uploader("사진을 올려주세요")
