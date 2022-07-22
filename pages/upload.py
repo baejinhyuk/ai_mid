29acdde023b3179055cd5e2bc4368469291dd3d3
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 09:33:24 +0900

    Update upload.py

--- pages/upload.py
+++ pages/upload.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import base64
+[theme]
+base="dark"
+
 st.warning('# 야채 15종 CNN')
 st.write("""
 ### 15개의 정해진 야채중 사진을 올려주세요!
 """)
 filename = st.file_uploader("사진을 올려주세요")
 st.markdown("![Alt Text](https://media.giphy.com/media/VJY3zeoK87CLBKnqqm/giphy.gif)")
-
-
-from PIL import Image
-image = Image.open('vae.jpg')
-
-st.image(image, caption='Sunrise by the mountains')
