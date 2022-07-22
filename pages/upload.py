910ce68d3a12ae9db16d434e32a0429ecdf0773c
baejinhyuk <81461966+baejinhyuk@users.noreply.github.com>
2022-07-22 13:38:14 +0900

    Update upload.py

--- pages/upload.py
+++ pages/upload.py
@@ -1,15 +1,4 @@
 import streamlit as st
-import base64
-import os
-import numpy as np
-import pickle
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import models,utils
-import pandas as pd
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img,img_to_array
-from tensorflow.python.keras import utils
 st.snow()
 
 st.warning('# 야채 15종 CNN')
